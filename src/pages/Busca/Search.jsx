20d3f58ac059a434b6e138ac4e122132d7b716e3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-12 10:26:11 -0300

    feat: create search page initial version

---
+++ src/pages/Busca/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Search = () => {
+  return (
+    <>
+      <h1>Essa é a página de busca</h1>
+      <Link to="/">Voltar</Link>
+    </>
+  );
+};
+
+export default Search;

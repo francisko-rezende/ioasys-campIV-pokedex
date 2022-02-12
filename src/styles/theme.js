10abfcd328db6fc038e55d3f9300299c90c372cc
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-12 09:22:27 -0300

    feat: create theme.js
    Creates theme.js containing the colors from figma

---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+const theme = {
+  colors: {
+    pokemonTypes: {
+      rock: "#B69E31",
+      ghost: "#70559B",
+      steel: "#B7B9D0",
+      water: "#6493EB",
+      grass: "#74CB48",
+      psychic: "#FB5584",
+      ice: "#9AD6DF",
+      dark: "#75574C",
+      fairy: "#E69EAC",
+      normal: "#AAA67F",
+      fighting: "#C12239",
+      flying: "#A891EC",
+      poison: "#A43E9E",
+      ground: "#DEC16B",
+      bug: "#A7B723",
+      fire: "#F57D31",
+      electric: "#F9CF30",
+      dragon: "#7037FF",
+    },
+    grayscale: {
+      darkGray: "#212121",
+      mediumGray: "#666666",
+      lightGray: "#E0E0E0",
+      white: "#FFFFFF",
+      background: "#F7F7F7",
+    },
+    ioasysColor: { secondaryColor: "#EC0344" },
+  },
+};
+
+export default theme;

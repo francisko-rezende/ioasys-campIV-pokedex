20c9e5a0fbfc3eab6e4c9ad66abb849f538c82f5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-21 10:47:56 -0300

    refactor: extract container to harbor main content from Fav Poke into its own component

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./Container.style";
+
+const Container = ({ children }) => {
+  return <S.Container>{children}</S.Container>;
+};
+
+export default Container;
---
+++ src/components/Container/Container.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 830px;
+  margin: 0 auto;
+`;

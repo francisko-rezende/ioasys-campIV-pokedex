b1efa16b1a1818e20d67f85c0af1a33f0389e802
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-21 10:10:33 -0300

    chore: remove console.log from Switch

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import * as S from "./Switch.style";
 
 const Switch = ({ onClick, mode }) => {
-  console.log(mode);
-
   return (
     <S.Switch onClick={onClick} mode={mode}>
       <S.Thumb mode={mode} />

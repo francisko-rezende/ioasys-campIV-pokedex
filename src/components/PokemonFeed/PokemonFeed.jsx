4a153017241096243d215c87ed47a10bf026d24d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-21 22:40:59 -0300

    refactor: replace forEach by simple calls in IntersectionObs and use flex for gallery in PokemonFeed

--- src/components/PokemonFeed/PokemonFeed.jsx
+++ src/components/PokemonFeed/PokemonFeed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import api from "../../services/api";
 import {
   GET_POKEMON_LIST,
@@ -17,12 +18,16 @@ const PokemonFeed = () => {
 
   React.useEffect(() => {
     const intersectionObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (!entry.isIntersecting) {
-          return;
-        }
-        dispatch(GET_POKEMON_LIST());
-      });
+      const [pageBottomEntry] = entries;
+      if (!pageBottomEntry.isIntersecting) return;
+      dispatch(GET_POKEMON_LIST());
+
+      // entries.forEach((entry) => {
+      //   if (!entry.isIntersecting) {
+      //     return;
+      //   }
+      //   dispatch(GET_POKEMON_LIST());
+      // });
     });
 
     intersectionObserver.observe(pageBottom.current);
@@ -35,44 +40,34 @@ const PokemonFeed = () => {
       Promise.all(pokemonList.map((name) => api.get(name))).then(
         (newResponses) => {
           const destructureData = ({ data }) => data;
-          const pokemonFeedData = newResponses.map(destructureData);
 
+          const pokemonFeedData = newResponses.map(destructureData);
           dispatch(UPDATE_POKEMON_FEED_DATA(pokemonFeedData));
         }
       );
     }
-  }, [pokemonList]);
+  }, [dispatch, pokemonList]);
 
   return (
-    <div>
+    <>
       {pokemonFeedData && (
-        <div>
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
-              gap: "20px",
-              maxWidth: "836px",
-            }}
-          >
-            {pokemonFeedData.map((pokemon) => (
-              <Card key={pokemon.id} {...pokemon} />
-            ))}
-          </div>
-        </div>
+        <Wrapper>
+          {pokemonFeedData.map((pokemon) => (
+            <Card key={pokemon.id} {...pokemon} />
+          ))}
+        </Wrapper>
       )}
-      <div
-        style={{
-          width: "calc(100vw - 15px)",
-          heigth: "10px",
-          background: "red",
-        }}
-        ref={pageBottom}
-      >
+      <div ref={pageBottom} style={{ height: "50px" }}>
         a
       </div>
-    </div>
+    </>
   );
 };
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
 
 export default PokemonFeed;

9bebc69e9d294c38a17de9dd585a33b93e604424
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-22 10:49:20 -0300

    refactor: create component for rendering pokemon list

---
+++ src/components/PokemonListContainer/PokemonListContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./PokemonListContainer.style.js";
+
+const PokemonListContainer = ({ children }) => {
+  return <S.Container>{children}</S.Container>;
+};
+
+export default PokemonListContainer;

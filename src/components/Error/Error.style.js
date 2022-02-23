d07bbd4e294a19bcb9dfcae430cf047beeeece96
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-23 19:10:13 -0300

    refactor: create a paragraph for the error text instead of using ::after (or before)

--- src/components/Error/Error.style.js
+++ src/components/Error/Error.style.js
@@ -1,41 +1,31 @@
 import styled from "styled-components";
 
-export const ErrorText = styled.p`
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ErrorText = styled.span`
   position: relative;
   font-size: 148px;
   font-weight: 700;
   margin-top: 29px;
   color: hsla(0, 0%, 74%, 1);
-
-  &::before {
-    content: "Este pokemon não foi encontrado ;(";
-    font-size: 22px;
-    color: hsla(0, 0%, 74%, 1);
-    font-weight: 600;
-    position: absolute;
-    right: -210px;
-    width: 351px;
-    bottom: -10px;
-  }
-
-  @media (max-width: 565px) {
-    &::before {
-      content: "Este pokemon não foi encontrado ;(";
-      font-size: 22px;
-      color: hsla(0, 0%, 74%, 1);
-      font-weight: 600;
-      position: absolute;
-      right: 0px;
-      text-align: center;
-      width: 351px;
-      bottom: -50px;
-      width: 100%;
-    }
-  }
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const MainMessage = styled.p`
+  font-size: 22px;
+  color: hsla(0, 0%, 74%, 1);
+  font-weight: 600;
+  position: absolute;
+  bottom: -10px;
+  left: 158px;
+  width: 351px;
+
+  @media (max-width: 565px) {
+    position: unset;
+    text-align: center;
+  }
 `;

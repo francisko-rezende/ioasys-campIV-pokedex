aa870a9bae5f4ece66b50454778c53141d19c5d1
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-27 09:53:46 -0300

    refactor: move search logic from Search to SearchBar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,59 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import api from "../../services/api";
 import MyFavoritesLink from "../MyFavoritesLink";
 import SearchBar from "../SearchBar";
 import SearchResult from "../SearchResult";
 import * as S from "./Search.style";
 
 const Search = () => {
-  const [searchedPokemon, setSearchedPokemon] = React.useState("");
   const [searchResult, setSearchResult] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
-
-  function handlePokemonSearch(e) {
-    e.preventDefault();
-
-    setSearchResult(null);
-    setError(null);
-    setIsLoading(true);
-
-    if (!searchedPokemon) {
-      return;
-    }
-
-    api
-      .get(`/pokemon/${searchedPokemon}`)
-      .then(({ data }) => {
-        setSearchResult(data);
-        setError(null);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setIsLoading(false);
-      });
-  }
-
-  const { currentMode } = useSelector(({ mode }) => mode);
 
   return (
     <>
       <S.Wrapper>
         <SearchBar
-          setSearchedPokemon={setSearchedPokemon}
-          handlePokemonSearch={handlePokemonSearch}
-          searchedPokemon={searchedPokemon}
-          mode={currentMode}
-          setError={setError}
           setSearchResult={setSearchResult}
+          setIsLoading={setIsLoading}
+          setError={setError}
         />
         <MyFavoritesLink />
       </S.Wrapper>
       <SearchResult
-        isLoading={isLoading}
         searchResult={searchResult}
+        isLoading={isLoading}
         error={error}
       />
     </>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import * as S from "./SearchBar.style";
 import { ReactComponent as SearchIcon } from "../../assets/icons/search-svgomg.svg";
+import api from "../../services/api";
+import { useSelector } from "react-redux";
 
-const SearchBar = ({
-  setSearchedPokemon,
-  handlePokemonSearch,
-  searchedPokemon,
-  mode,
-  setError,
-  setSearchResult,
-}) => {
+const SearchBar = ({ setSearchResult, setError, setIsLoading }) => {
+  const [searchedPokemon, setSearchedPokemon] = React.useState("");
+
+  function handlePokemonSearch(e) {
+    e.preventDefault();
+
+    setSearchResult(null);
+    setError(null);
+    setIsLoading(true);
+
+    if (!searchedPokemon) {
+      return;
+    }
+
+    api
+      .get(`/pokemon/${searchedPokemon}`)
+      .then(({ data }) => {
+        setSearchResult(data);
+        setError(null);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }
+
   const resetSearchAndError = () => {
     setError(null);
     setSearchResult(null);
     setSearchedPokemon("");
   };
 
+  const { currentMode } = useSelector(({ mode }) => mode);
+
   return (
     <>
       <S.Form onSubmit={handlePokemonSearch}>
-        <S.Label htmlFor="pokemonSearch" mode={mode}>
+        <S.Label htmlFor="pokemonSearch" mode={currentMode}>
           Buscar
         </S.Label>
         <S.Input

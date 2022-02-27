9bb8814dd1d3c78e51c1498fa8cb9c5894166599
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-27 12:23:19 -0300

    fix: remove key prop from PokemonTypeTag thus fixing warning "key is not prop"

--- src/components/PokemonTypeTag/PokemonTypeTag.jsx
+++ src/components/PokemonTypeTag/PokemonTypeTag.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import * as S from "./PokemonTypeTag.style";
 
-const PokemonTypeTag = ({ key, pokemonType }) => {
+const PokemonTypeTag = ({ pokemonType }) => {
   return (
-    <S.PokemonTypeTag key={key} pokemonType={pokemonType}>
-      {pokemonType}
-    </S.PokemonTypeTag>
+    <S.PokemonTypeTag pokemonType={pokemonType}>{pokemonType}</S.PokemonTypeTag>
   );
 };
 

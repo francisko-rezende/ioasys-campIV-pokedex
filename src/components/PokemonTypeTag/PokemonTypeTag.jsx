573b34bdb04f93710136708959dd2317988a2e98
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-28 13:54:01 -0300

    feat: implement propTypes to PokemonTypeTag

--- src/components/PokemonTypeTag/PokemonTypeTag.jsx
+++ src/components/PokemonTypeTag/PokemonTypeTag.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import * as S from "./PokemonTypeTag.style";
+import PropTypes from "prop-types";
 
 const PokemonTypeTag = ({ pokemonType }) => {
   return (
     <S.PokemonTypeTag pokemonType={pokemonType}>{pokemonType}</S.PokemonTypeTag>
   );
+};
+
+PokemonTypeTag.propTypes = {
+  pokemonType: PropTypes.oneOf([
+    "rock",
+    "ghost",
+    "steel",
+    "water",
+    "grass",
+    "psychic",
+    "ice",
+    "dark",
+    "fairy",
+    "normal",
+    "fighting",
+    "flying",
+    "poison",
+    "ground",
+    "bug",
+    "fire",
+    "electric",
+    "dragon",
+  ]),
 };
 
 export default PokemonTypeTag;

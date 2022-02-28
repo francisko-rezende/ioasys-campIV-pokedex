a959949d9e3ec55b9631949cc9696d041518e7e3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-28 13:50:27 -0300

    feat: make storybook search for stories in src/components as well

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,16 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": "@storybook/react",
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
   core: {
-    builder: "webpack5"
-  }
+    builder: "webpack5",
+  },
 };

8bc783a0e95f0ff45d012a771f3a395b76e0dab0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-28 19:37:16 -0300

    chore: clean up routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritePokemon from "./pages/Favoritos/FavoritePokemon";
-// import App from "./App";
 import PokemonDetails from "./pages/PokemonDetails";
 
 function Navigation() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<App />} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/:name" element={<PokemonDetails />} />
         <Route path="/favoritos" element={<FavoritePokemon />} />
         <Route path="/favoritos/:name" element={<PokemonDetails />} />
-        {/* <Route path="/sobre" element={<PokemonDetails />} /> */}
       </Routes>
     </Router>
   );

a0a8d668620c2900dffdb903d6893d9022342929
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-02-28 18:37:00 -0300

    feat: write unitary tests for PokemonTypeTag

---
+++ src/components/PokemonTypeTag/PokemonTypeTag.test.js
@@ -0,0 +1,26 @@
+import PokemonTypeTag from "./";
+import { render, screen } from "@testing-library/react";
+import theme from "../../styles/theme";
+import { ThemeProvider } from "styled-components";
+
+test("tag renders the received pokemon type as text properly", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <PokemonTypeTag pokemonType="fire" />
+    </ThemeProvider>
+  );
+
+  const tag = screen.getByText(/fire/i);
+  expect(tag).toBeInTheDocument();
+});
+
+test("tag's background color renders as expected", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <PokemonTypeTag pokemonType="dragon" />
+    </ThemeProvider>
+  );
+
+  const tag = screen.getByText(/dragon/i);
+  expect(tag).toHaveStyle({ backgroundColor: "#7037FF" });
+});
